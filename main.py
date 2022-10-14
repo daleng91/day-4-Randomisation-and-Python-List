fe99af0ba51309048994fa4e274a9c2320599809
Nguyễn Hoàng <>
2022-10-14 22:43:40 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,57 @@
+import random
+
+# random_int = random.randint(0,5)
+# print(random_int)
+# random_float = random.random() + random.randint(0,4)
+# print(random_float)
+#How to create a random decimal number between 0 and 5
+
+# random_num = random.randint(0,1)
+
+# if random_num == 0:
+#   print("Heads")
+# else:
+#   print("Tails")
+
+# people_name = input("Give me everybody's name, seperated by a comma.")
+# list_of_name = people_name.split(", ")
+# print (list_of_name)
+# num_of_items = len(list_of_name) - 1
+
+# print(num_of_items)
+# random_no = random.randint(0,num_of_items)
+# print(f"{list_of_name[random_no]} is going to buy the meal today")
+
+#my_friends = ['Dale', 'Tommy','Shane','Ping','Roshan','Winston']
+# print(my_friends)
+# for i in range(5):
+#   print(random.choice(my_friends))
+
+# fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+# fruits[-1] = "Melons"
+# fruits.append("Lemons")
+# print(fruits)
+
+# fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+# vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+
+# dirty_dozen = (fruits,vegetables)
+
+# print(dirty_dozen)
+
+# print(dirty_dozen[1][1])
+
+#Day 4.3 Treasure map
+row1 = ["@","@","@"]
+row2 = ["@","@","@"]
+row3 = ["@","@","@"]
+
+print(row1)
+print(row2)
+print(row3)
+
+location = input("Please choose your choice with location (column,row): ")
+location_len = len(location)
+tmp = split(location_len)
+
+print(tmp)
